3e6e1450ae89ffd99cef428dd12b32cd9300bfef
Foundy <foundy@foundy.io>
2017-03-09 20:16:49 +0900

    Add new elements

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-var package1 = function () {
-  console.log('package1 !!');
-};
+module.exports = function () {
+  var span = document.createElement('span');
+  span.setAttribute('style', 'color:blue');
+  span.textContent = 'Component : npm-test-pakcage1';
 
-module.exports = package1;
+  var div = document.createElement('div');
+  div.appendChild(span);
+
+  return div;
+};
